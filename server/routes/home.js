f9823ac29ccca9de7879eb9ba0be2031a4beb25a
Nathan-Wang23 <nathan.wang2323@gmail.com>
2021-11-16 18:39:37 -0500

    adding get profile from spotify api

--- server/routes/home.js
+++ server/routes/home.js
@@ -4,9 +4,29 @@ const request = require('request');
 const config = require('../config');
 
 router.get('/', (req, res) => {
-  res.send({
-    "email" : "not logged in"
-  });
+  let BASE_URL = 'https://api.spotify.com/v1/';
+  let headers = {'Authorization': 'Bearer ' + config.access_token}
+  if (config.access_token != "") {
+    request(
+      {
+        method: "GET",
+        url: BASE_URL + "me",
+        headers: headers,
+        json: true,
+      }, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          console.log("Getting Profile");
+          console.log(body);
+          console.log(body['display_name']);
+          res.send(
+            {
+              'email' : body['display_name']
+            });
+        } else {
+          console.log("Error fetching profile");
+        }
+    });
+  }
 });
 
 module.exports = router;
